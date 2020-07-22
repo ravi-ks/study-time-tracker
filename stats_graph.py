6e453d8b426e72aac33db57d1e0efd878ea8a04a
Ravi <37482890+ravi-ks@users.noreply.github.com>
2020-07-22 23:26:54 +0530

    components of version 3, a stats tool

--- stats_graph.py
+++ stats_graph.py
@@ -1,23 +1,27 @@
 import matplotlib.pyplot as plt 
-f = open("study_logs.txt", "r")
-i = 1
-j = 1
-k = 3
-x = []
-y = []
-for lines in f:
-        if i == j:
-                j += 4
-                parts = lines.split()
-                x.append(int(parts[1]))
-        elif i == k:
-                k += 4
-                parts = lines.split(":")
-                y.append((int(parts[1].strip()) * 60) + int(parts[2]))
-        i += 1
-plt.plot(x, y, color='black', linestyle='dashed', linewidth = 3, 
-         marker='o', markerfacecolor='grey', markersize=8) 
-plt.xlabel("day number")
-plt.ylabel("time studied (in mins)")
-plt.title("study statistics")
-plt.show()
+import os
+if os.stat("study_logs.txt").st_size == 0:
+        print("study_logs.txt is empty.\nNo data to plot on.")
+else:
+        f = open("study_logs.txt", "r")
+        i = 1
+        j = 1
+        k = 3
+        x = []
+        y = []
+        for lines in f:
+                if i == j:
+                        j += 4
+                        parts = lines.split()
+                        x.append(int(parts[1]))
+                elif i == k:
+                        k += 4
+                        parts = lines.split(":")
+                        y.append((int(parts[1].strip()) * 60) + int(parts[2]))
+                i += 1
+        plt.plot(x, y, color='black', linestyle='dashed', linewidth = 3, 
+                 marker='o', markerfacecolor='grey', markersize=8) 
+        plt.xlabel("day number")
+        plt.ylabel("time studied (in mins)")
+        plt.title("study statistics")
+        plt.show()
